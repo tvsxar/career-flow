5ca4ea2d02a15d57609304af8e70a2bc8894c16a
tvsxar <tarasofficial82@gmail.com>
2026-07-28 12:55:55 +0300

    feat: connect application to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 1099;
+
+await connectDB();
 
 app.use(cors());
 app.use(express.json());
